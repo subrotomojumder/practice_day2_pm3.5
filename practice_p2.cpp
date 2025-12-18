3169f5b97ce3a093a25e25e3facaa578d0d228b9
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-12-18 19:29:17 +0600

    extra plm 2 solved

--- practice_p2.cpp
+++ practice_p2.cpp
@@ -7,13 +7,18 @@ int main()
     {
     public:
         int jersey_no;
-        // char country[100];
+        char country[100];
     };
-    Cricketer * dhoni;
+    Cricketer *dhoni = new Cricketer;
     dhoni->jersey_no = 10;
-    // char country_name[100] = "India";
-    // strcpy(dhoni->country, country_name);
+    char country_name[100] = "India";
+    strcpy(dhoni->country, country_name);
 
-    cout << dhoni->jersey_no;
+    Cricketer *kohli = new Cricketer;
+    // kohli = dhoni; // wrong way
+    strcpy(kohli->country, dhoni->country);
+    kohli->jersey_no = dhoni->jersey_no;
+    delete dhoni;
+    cout << kohli->country;
     return 0;
 }
